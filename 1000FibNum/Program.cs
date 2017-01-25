a9ee4a71322b14c0b6e953e1c5925d1b872b3ad4
Narcarti <tcmarkymark@gmail.com>
2017-01-25 10:26:14 -0500

    Initial upload of 1000FibNum V1

---
+++ 1000FibNum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _1000FibNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibcnt = 1;
+            BigInteger fnum = 0;
+            BigInteger snum = 1;
+            BigInteger fibnum = 0;
+
+            while(fibnum.ToString().Length != 1000)
+            {
+                fibnum = fnum + snum;
+                fnum = snum;
+                snum = fibnum;
+
+                fibcnt++;
+            }
+            Console.WriteLine(fibnum);
+            Console.WriteLine(fibcnt);
+            Console.ReadKey();
+        }
+    }
+}
